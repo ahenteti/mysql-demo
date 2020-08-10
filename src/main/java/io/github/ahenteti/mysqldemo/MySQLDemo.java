6a7de65d9589a41718eb2a1dc751cd6f442659ed
ahenteti <ahmad.henteti@gmail.com>
2020-08-11 00:00:01 +0200

    add create SQL procedures

--- src/main/java/io/github/ahenteti/mysqldemo/MySQLDemo.java
+++ src/main/java/io/github/ahenteti/mysqldemo/MySQLDemo.java
@@ -7,11 +7,15 @@ import java.sql.Statement;
 
 public class MySQLDemo {
 
+    public static final String DB_NAME = "DB_USER";
+
     public static void main(String[] args) {
         try {
             createDatabase();
             createTable();
             insertData();
+            createGetUsersCountProcedure();
+            createGetUsersAverageAgeProcedure();
             deleteDatabase();
         } catch (Exception e) {
             e.printStackTrace();
@@ -20,31 +24,59 @@ public class MySQLDemo {
 
     private static void createDatabase() throws SQLException {
         try (Connection connection = DriverManager.getConnection(getDbUrl())) {
-            String query = "CREATE DATABASE DB_USER";
-            executeStatement(connection, query);
+            String query = "CREATE DATABASE " + DB_NAME;
+            executeAndUpdateStatement(connection, query);
         }
     }
 
     private static void createTable() throws SQLException {
-        try (Connection connection = DriverManager.getConnection(getDbUrl("DB_USER"))) {
-            String query = "CREATE TABLE T_USER (id INTEGER not NULL, age INTEGER not NULL, PRIMARY KEY ( id ))";
-            executeStatement(connection, query);
+        try (Connection connection = DriverManager.getConnection(getDbUrl(DB_NAME))) {
+            String query = "CREATE TABLE T_USERS (id INTEGER not NULL, age INTEGER not NULL, PRIMARY KEY ( id ))";
+            executeAndUpdateStatement(connection, query);
         }
     }
 
     private static void insertData() throws SQLException {
-        try (Connection connection = DriverManager.getConnection(getDbUrl("DB_USER"))) {
-            String query = "INSERT INTO T_USER (id, age) VALUES (1, 12)";
+        try (Connection connection = DriverManager.getConnection(getDbUrl(DB_NAME))) {
+            String query = "INSERT INTO T_USERS (id, age) VALUES (1, 12)";
+            executeAndUpdateStatement(connection, query);
+            query = "INSERT INTO T_USERS (id, age) VALUES (2, 13)";
+            executeAndUpdateStatement(connection, query);
+        }
+    }
+
+    private static void createGetUsersCountProcedure() throws SQLException {
+        try (Connection connection = DriverManager.getConnection(getDbUrl(DB_NAME))) {
+            // @formatter:off
+            String query = ""
+                + "CREATE PROCEDURE GetUsersCount(OUT counter INT)\n"
+                + "BEGIN\n"
+                + "    SELECT count(*) INTO counter\n"
+                + "    FROM T_USERS;\n"
+                + "END";
+            // @formatter:on
             executeStatement(connection, query);
-            query = "INSERT INTO T_USER (id, age) VALUES (2, 13)";
+        }
+    }
+
+    private static void createGetUsersAverageAgeProcedure() throws SQLException {
+        try (Connection connection = DriverManager.getConnection(getDbUrl(DB_NAME))) {
+            // @formatter:off
+            String query = ""
+                + "CREATE PROCEDURE GetUsersAverageAge(OUT counter INT)\n"
+                + "BEGIN\n"
+                + "    SELECT avg(age) INTO counter\n" 
+                + "    FROM T_USERS;\n"
+                + "END";
+            // @formatter:on
             executeStatement(connection, query);
         }
     }
 
     private static void deleteDatabase() throws SQLException {
         try (Connection connection = DriverManager.getConnection(getDbUrl())) {
-            String query = "DROP DATABASE DB_USER";
-            executeStatement(connection, query);
+            String query = "DROP DATABASE " + DB_NAME;
+            executeAndUpdateStatement(connection, query);
         }
     }
 
@@ -56,9 +88,15 @@ public class MySQLDemo {
         return String.format("jdbc:mysql://localhost:3306/%s?user=root&password=mysql&serverTimezone=UTC", database);
     }
 
-    private static void executeStatement(Connection connection, String query) throws SQLException {
+    private static void executeAndUpdateStatement(Connection connection, String query) throws SQLException {
         try (Statement statement = connection.createStatement()) {
             statement.executeUpdate(query);
+        }
+    }
+
+    private static void executeStatement(Connection connection, String query) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(query);
         }
     }
 }
